46f9364399bef1c1de1470434cf7145849d5ab98
Manideep-0164 <peddaboinimanideep03@gmail.com>
2023-06-29 21:25:48 +0530

    .

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("./tmp/db.json");
+// const router = jsonServer.router("./tmp/db.json");
+const fs = require('fs')
+const path = require("path")
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
+const router = jsonServer.router(db)
 const cors = require("cors");
 const middlewares = jsonServer.defaults();
 
@@ -8,6 +12,7 @@ server.use(cors());
 server.use(middlewares);
 server.use(router);
 
-server.listen(2000, ()=>{
+server.listen(4000, ()=>{
     console.log('JSON Server is running at port: 2000')
 })
+ console.log(path.join(__dirname, 'db.json'))
