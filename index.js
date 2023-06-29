37c574a1c7bb33359187bdf7b7744e2a1a7f9d8c
Manideep-0164 <peddaboinimanideep03@gmail.com>
2023-06-29 19:32:28 +0530

    created backend

---
+++ index.js
@@ -0,0 +1,13 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./db.json");
+const cors = require("cors");
+const middlewares = jsonServer.defaults();
+
+server.use(cors());
+server.use(middlewares);
+server.use(router);
+
+server.listen(2000, ()=>{
+    console.log('JSON Server is running at port: 2000')
+})
