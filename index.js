7534e1da7c22841eaf2a5084b0e9dad4493edfc0
Manideep-0164 <peddaboinimanideep03@gmail.com>
2023-06-30 01:44:38 +0530

    route - added login route

--- index.js
+++ index.js
@@ -11,9 +11,38 @@ const middlewares = jsonServer.defaults();
 // router.db.readOnly = false;
 server.use(cors());
 server.use(middlewares);
+
+server.use(jsonServer.bodyParser)
+
+server.post("/login", (req, res) => {
+    const payload = req.body
+
+    try {
+        const post = router.db.get('users').find({ Email: payload.Email }).value();
+
+        if (!post) return res.status(404).json({
+            message: "User not found, Please Register"
+        });
+
+        if (post.Password !== payload.Password) return res.status(400).json({
+            message: "Email/Password Incorrect"
+        });
+
+        res.status(200).json({
+            message: "Login Success",
+            Username: post.Username
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            error: "Something went wrong"
+        })
+        console.log(error)
+    }
+})
+
 server.use(router);
 
-server.listen(4000, ()=>{
-    console.log('JSON Server is running at port: 2000')
+server.listen(4000, () => {
+    console.log('JSON Server is running at port: 4000')
 })
-//  console.log(path.join(__dirname, 'db.json'))
